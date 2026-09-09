de4613ef162cab1823835e4b7da84f74bb61eef0
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-09 20:40:13 +1000

    feat: add java spring boot as the backend

---
+++ backend/src/main/java/com/example/kali/KaliApplication.java
@@ -0,0 +1,13 @@
+package com.example.kali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaliApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/kali/KaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

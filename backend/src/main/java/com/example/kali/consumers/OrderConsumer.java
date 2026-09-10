27e74183e01a08442c77382dfbb01d2f6ac1eeec
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-10 21:18:34 +1000

    fix: add jackson databind dependencies to resolve JSON seriliazation/deseriliazation

--- backend/src/main/java/com/example/kali/consumers/OrderConsumer.java
+++ backend/src/main/java/com/example/kali/consumers/OrderConsumer.java
@@ -1,15 +1,30 @@
 package com.example.kali.consumers;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.example.kali.models.Order;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor 
 public class OrderConsumer {
+
+    private final KafkaTemplate<String, Order> kafkaTemplate;
+
+    private static final String STATUS_TOPIC = "order-status";
     
     @KafkaListener(topics = "orders", groupId = "kali-order-group")
-    public void consume(Order order) {
-        System.out.println("Received order: " + order);
+    public void consume(Order order) throws InterruptedException {
+        System.out.println("Processing order: " + order.getId());
+
+        Thread.sleep(3000);
+
+        order.setStatus("COMPLETED");
+
+        kafkaTemplate.send(STATUS_TOPIC, order.getId(), order);
+        System.out.println("Order completed: " + order.getId());
     }
 }

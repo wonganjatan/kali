7dd5aebec6f8ec9b0b522a9aa0eebf39ba9fe172
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-10 21:56:46 +1000

    feat: add get ammping to get all orders and find specific order

--- backend/src/main/java/com/example/kali/controllers/OrderController.java
+++ backend/src/main/java/com/example/kali/controllers/OrderController.java
@@ -1,9 +1,12 @@
 package com.example.kali.controllers;
 
 import java.time.Instant;
+import java.util.Collection;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.kali.models.Order;
 import com.example.kali.producers.OrderProducer;
+import com.example.kali.stores.OrderStore;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 public class OrderController {
     
     private final OrderProducer orderProducer;
+    private final OrderStore orderStore;
 
     @PostMapping 
     public ResponseEntity<Order> createOrder(@RequestBody Order incomingOrder) {
@@ -32,6 +37,21 @@ public class OrderController {
         );
 
         orderProducer.sendOrder(order);
+
+        return ResponseEntity.ok(order);
+    }
+
+    @GetMapping 
+    public ResponseEntity<Collection<Order>> getAllOrders() {
+        return ResponseEntity.ok(orderStore.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> getOrderById(@PathVariable String id) {
+        Order order = orderStore.findById(id);
+        if (order == null) {
+            return ResponseEntity.notFound().build();
+        }
 
         return ResponseEntity.ok(order);
     }
